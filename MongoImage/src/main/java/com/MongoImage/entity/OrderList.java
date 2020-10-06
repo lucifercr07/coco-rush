cfb154ac3b4e6ca89917eb4664eb2724ae4a0b4a
nishant kumar <nishant.kumarpat1996@gmail.com>
2020-10-06 12:28:43 +0530

    added java code

---
+++ MongoImage/src/main/java/com/MongoImage/entity/OrderList.java
@@ -0,0 +1,8 @@
+package com.MongoImage.entity;
+
+public class OrderList {
+String productId;
+int unitPrice;
+int quantity;
+int totalPrice;
+}
---
+++ MongoImage/src/test/java/com/MongoImage/MongoImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MongoImage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
