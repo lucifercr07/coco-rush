b14094ce34a2dacd10ccbf2dbfe14d925083f924
Arka Naskar <naskara97@gmail.com>
2020-11-07 13:41:58 +0530

    fix: lint

--- cc.web/src/views/Cart/Component/PromoCode.jsx
+++ cc.web/src/views/Cart/Component/PromoCode.jsx
@@ -3,32 +3,32 @@ import { TextField, Grid, Button } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 
 const PromoTextField = withStyles({
-	root: {
-		paddingRight: '1em',
-		'& .MuiOutlinedInput-root': {
-			borderRadius: 0,
-			'&:hover fieldset': {
-				borderColor: '#c58c4a',
-			},
-			'&.Mui-focused fieldset': {
-				borderColor: '#654321',
-			},
-		},
-	},
+  root: {
+    paddingRight: '1em',
+    '& .MuiOutlinedInput-root': {
+      borderRadius: 0,
+      '&:hover fieldset': {
+        borderColor: '#c58c4a',
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: '#654321',
+      },
+    },
+  },
 })(TextField);
 
 export default function PromoCode(props) {
-	return <>
-		<Grid item xs={8}>
-			<PromoTextField
-				placeholder="Enter Coupon Code"
-				fullWidth={true}
-				variant="outlined"
-				size="small"
-			/>
-		</Grid>
-		<Grid item xs={4}>
-			<Button variant="contained" fullWidth={true} className="primary-button" >Apply</Button>
-		</Grid>
-	</>
+  return <>
+    <Grid item xs={8}>
+      <PromoTextField
+        placeholder="Enter Coupon Code"
+        fullWidth={true}
+        variant="outlined"
+        size="small"
+      />
+    </Grid>
+    <Grid item xs={4}>
+      <Button variant="contained" fullWidth={true} className="primary-button" >Apply</Button>
+    </Grid>
+  </>
 }
